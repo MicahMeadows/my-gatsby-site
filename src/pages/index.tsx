9cfaa4044ca7fe7ca8729a58da9e9bed4a404900
MicahMeadows <micahm213@gmail.com>
2022-09-14 16:04:09 -0400

    Finish part 5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Link, HeadFC } from "gatsby";
 import Layout from '../components/layout';
 import { StaticImage } from 'gatsby-plugin-image';
+import Seo from '../components/seo';
 
 // Step 2: Define your component
 const IndexPage = () => {
@@ -14,7 +15,7 @@ const IndexPage = () => {
 }
 
 // You'll learn about this in the next task, just copy it for now
-export const Head : HeadFC = () => <title>Home Page</title>
+export const Head : HeadFC = () => <Seo title="Home"/>;
 
 // Step 3: Export your component
 export default IndexPage
